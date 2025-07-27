36f391ee9d91f3104b3b0215bf024717af920e1e
Liaqat-dev <dev.liaqat13@gmail.com>
2025-07-27 22:21:13 +0500

    fix padding

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -5,7 +5,7 @@ import {motion} from 'framer-motion';
 
 export default function Skills() {
     return (
-        <section id="skills" className="py-16 bg-black text-white">
+        <section id="skills" className="py-16 bg-gradient-to-bl from-gray-800 via-black to-gray-950 text-white">
             <motion.div
                 initial={{opacity: 0, y: 30}}
                 whileInView={{opacity: 1, y: 0}}
